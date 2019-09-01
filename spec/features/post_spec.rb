6a3822107e589b810f8d62933b634a0f84cefd6e
Abagail Hudson <abagailanne@gmail.com>
2019-09-01 19:05:27 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -19,9 +19,9 @@ require 'rails_helper'
     fill_in 'post_title', with: 'My post title'
     fill_in 'post_description', with: 'My post description'
 
-     click_on 'Submit Post'
+    click_on 'Submit Post'
 
-     expect(page.current_path).to eq(posts_path)
+    expect(page.current_path).to eq(posts_path)
     expect(page).to have_content('My post title')
     expect(page).to have_content('My post description')
   end
